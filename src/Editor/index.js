dc2920131b847f4a4dfb779ca17be0bd64c8cbdf
Enin Fujimi <tkgm45nbchk@gmail.com>
2020-12-17 04:25:57 +0900

    Trying to change CSS and saying Hello World was succeeded

---
+++ src/Editor/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Editor = () => {
+    // const h1Style = { color: "#fff" };
+    return (
+        <h1>Hello World!!</h1>
+        // <h1 style={h1Style} >Hello World!!</h1>
+    );
+}
+
+export default Editor;
---
+++ src/static/style.js
@@ -0,0 +1,15 @@
+export default <style>{`
+    html {
+        font-size: 16px;
+    }
+    h1 {
+        font-size: 6rem;
+        color: #ccc;
+    }
+
+    @media (min-width:800px) {
+        img.top {
+            width: 1000px;
+        }
+    }
+`}</style>
